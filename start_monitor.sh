01835ae4ccac3104923e7a417696cb7921643ee5
ChingHuaYang <chinghua.yang@appier.com>
2018-01-22 01:02:38 +0800

    add call server script

---
+++ start_monitor.sh
@@ -0,0 +1,1 @@
+python3 webcam_capture_DVR.py --ip localhost:5566 -o /MAS/pics/ --timesleep 10
---
+++ start_server.sh
@@ -0,0 +1,1 @@
+python3 ./bottle_test_yolo.py model_data/yolo.h5 --ip localhost:5566 -o /MAS/pics/ -s 0.68
